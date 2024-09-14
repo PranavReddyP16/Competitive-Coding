07db05dd94969ab6471c5e5933cf098282bde40b
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-09-14 15:36:24 -0500

    Add ABC B

---
+++ AtCoder/ABC/ABC350/B.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+signed main(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;sort(a.begin(),a.end());for(int x:a)cout<<x<<" ";}
