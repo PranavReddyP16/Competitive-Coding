9f9767d1f2854516459ad16145317cbd3c665706
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-09-28 19:03:18 -0500

    Add ABC A

---
+++ AtCoder/ABC/ABC395/A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    sort(a.begin(),a.end());
+    int ans=3254669779 % 1000;
+    for(int i=0;i<n;i++) ans+=a[i]*(i+1);
+    cout<<ans<<endl;
+}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 8261000
