b859376b3460678033fbc84e20c3db1ce27a8414
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-10-19 18:47:01 -0500

    Add ARC A

---
+++ AtCoder/ARC/ARC178/A.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+const int MOD=998244353;
+signed main(){int n;cin>>n;int ans=1;for(int i=0;i<n;i++)ans=ans*2%MOD;cout<<ans<<"\n";}
