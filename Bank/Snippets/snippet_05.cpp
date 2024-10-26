a546ce31520ebcb74ad870fd0d226a16ba682ded
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-10-26 18:10:38 -0500

    Add solution snippet snippet_05

---
+++ Bank/Snippets/snippet_05.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int g=a[0];for(int i=1;i<n;i++)g=__gcd(g,a[i]);cout<<g<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
