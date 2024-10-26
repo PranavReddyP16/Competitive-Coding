bf0ad99f2016dae4a130a9a1acbe5b9b42d405aa
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-10-26 10:26:19 -0500

    Add solution snippet snippet_03

---
+++ Bank/Snippets/snippet_03.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    sort(a.begin(),a.end());int c=0;for(int i=1;i<n;i++)if(a[i]==a[i-1])c++;cout<<c<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
