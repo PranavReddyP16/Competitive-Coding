5773bda1f5aac0bb1aa08cae79d234385ebf4aac
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-10-26 22:28:12 -0500

    Add solution snippet snippet_06

---
+++ Bank/Snippets/snippet_06.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    string s;cin>>s;string t=s;reverse(t.begin(),t.end());cout<<(s==t?"YES":"NO")<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
