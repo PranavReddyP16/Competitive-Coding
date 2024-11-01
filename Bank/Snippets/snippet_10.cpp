4196208edb73be7e637d4e3b7c1b6a39945b9fbf
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-10-31 20:40:40 -0500

    Add solution snippet snippet_10

---
+++ Bank/Snippets/snippet_10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int s=0;for(int x:a)if(x%2==0)s+=x;cout<<s<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
