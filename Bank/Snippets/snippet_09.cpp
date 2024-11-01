de05ba7b400d44132fb8e0fcd931a794d2ab6346
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-10-31 19:52:30 -0500

    Add solution snippet snippet_09

---
+++ Bank/Snippets/snippet_09.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int x;cin>>x;bool ok=x>1;for(int i=2;i*i<=x;i++)if(x%i==0)ok=false;cout<<(ok?"YES":"NO")<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
