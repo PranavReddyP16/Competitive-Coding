bbe3cc444b250d1195b46f05c530b03a0e0b230b
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-11-01 16:50:00 -0500

    Add solution snippet snippet_11

---
+++ Bank/Snippets/snippet_11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    reverse(a.begin(),a.end());for(int x:a)cout<<x<<" ";cout<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
