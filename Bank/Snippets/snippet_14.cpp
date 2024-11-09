46f8be1d52a13f57bec256813462ae7741d59aec
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-11-09 15:10:48 -0500

    Add solution snippet snippet_14

---
+++ Bank/Snippets/snippet_14.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int m;cin>>m;vector<int>b(m);for(auto&x:b)cin>>x;vector<int>c;merge(a.begin(),a.end(),b.begin(),b.end(),back_inserter(c));for(int x:c)cout<<x<<" ";cout<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
