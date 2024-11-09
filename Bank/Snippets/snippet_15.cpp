bcffc3f7ca2f0d96da8acd0ded991f3d15ac64d5
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-11-09 16:12:17 -0500

    Add solution snippet snippet_15

---
+++ Bank/Snippets/snippet_15.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int s=n*(n+1)/2;for(int x:a)s-=x;cout<<s<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
