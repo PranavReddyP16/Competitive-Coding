89b26bd3df1938d03377fbe7f71e6ce572675765
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-11-09 21:29:33 -0500

    Add solution snippet snippet_17

---
+++ Bank/Snippets/snippet_17.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int lo=0,hi=n-1;while(lo<hi){if(a[lo]==0)lo++;else if(a[hi]==2)hi--;else if(a[lo]==2&&a[hi]==0){swap(a[lo],a[hi]);lo++;hi--;}else lo++;}for(int x:a)cout<<x<<" ";cout<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
