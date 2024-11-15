ac3125930f67cc8976ebcce225ea81feef3e4465
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-11-14 22:44:18 -0500

    Add solution snippet snippet_18

---
+++ Bank/Snippets/snippet_18.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int mn=a[0],mx=0;for(int x:a){mn=min(mn,x);mx=max(mx,x-mn);}cout<<mx<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
