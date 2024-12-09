5f03fb9a27551e4101473312e6e3d2a35bc9984b
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-12-09 18:05:19 -0500

    Add solution snippet snippet_26

---
+++ Bank/Snippets/snippet_26.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    string s;cin>>s;int f[256]={};for(char c:s)f[(int)c]++;for(char c:s)if(f[(int)c]==1){cout<<c<<"
+";return;}cout<<"-1
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
