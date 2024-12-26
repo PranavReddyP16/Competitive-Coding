f340b184e44099cc79471d779c7a6544e3669037
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2024-12-25 20:16:43 -0500

    Add solution snippet snippet_33

---
+++ Bank/Snippets/snippet_33.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    sort(a.begin(),a.end());int x;cin>>x;auto it=lower_bound(a.begin(),a.end(),x);int fl=it!=a.begin()?*prev(it):-1;int cl=it!=a.end()?*it:-1;cout<<fl<<" "<<cl<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
