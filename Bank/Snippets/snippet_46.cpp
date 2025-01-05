1b30306c9b84f2a65dced18de3fa8bcf722ac4f7
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-01-05 16:56:50 -0500

    Add solution snippet snippet_46

---
+++ Bank/Snippets/snippet_46.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    string s,t;cin>>s>>t;int m=s.size(),k=t.size();vector<vector<int>>dp(m+1,vector<int>(k+1,0));for(int i=0;i<=m;i++)dp[i][0]=i;for(int j=0;j<=k;j++)dp[0][j]=j;for(int i=1;i<=m;i++)for(int j=1;j<=k;j++){if(s[i-1]==t[j-1])dp[i][j]=dp[i-1][j-1];else dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});}cout<<dp[m][k]<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
