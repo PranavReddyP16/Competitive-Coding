941b7c0811fbb88f837767d5241440dcad89f2ae
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-01-05 10:58:19 -0500

    Add solution snippet snippet_42

---
+++ Bank/Snippets/snippet_42.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int x;cin>>x;vector<int>dp(x+1);dp[0]=1;if(x>=1)dp[1]=1;for(int i=2;i<=x;i++)dp[i]=dp[i-1]+dp[i-2];cout<<dp[x]<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
