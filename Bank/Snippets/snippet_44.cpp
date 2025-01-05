8964d789022b229c30150830d7786ca47f738adf
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-01-05 13:46:17 -0500

    Add solution snippet snippet_44

---
+++ Bank/Snippets/snippet_44.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    vector<int>dp;for(int x:a){auto it=lower_bound(dp.begin(),dp.end(),x);if(it==dp.end())dp.push_back(x);else *it=x;}cout<<dp.size()<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
