9c22ae066df879060affa30cf25e42a7a5d43911
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-01-11 20:53:20 -0500

    Add problem cf2108_A

---
+++ Bank/Unsolved/cf2108_A.cpp
@@ -0,0 +1,12 @@
+// dest: Codeforces/Div3/CF2108/
+// problem: CF2108 A
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    // TODO
+}
+signed main(){int t;cin>>t;while(t--)solve();}
