c7ef613d900fb8ea8d6a918c9a9b2e54efe9a94d
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-01-19 11:15:27 -0500

    Add problem cf2119_B

---
+++ Bank/Unsolved/cf2119_B.cpp
@@ -0,0 +1,13 @@
+// dest: Codeforces/Div2/CF2119/
+// problem: CF2119 B
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    // TODO
+}
+signed main(){int t;cin>>t;while(t--)solve();}
