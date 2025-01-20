dca59d277db482f19af93ee7a10791333f29b106
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-01-20 18:13:29 -0500

    Add problem cf2123_A

---
+++ Bank/Unsolved/cf2123_A.cpp
@@ -0,0 +1,12 @@
+// dest: Codeforces/Div3/CF2123/
+// problem: CF2123 A
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    // TODO
+}
+signed main(){int t;cin>>t;while(t--)solve();}
