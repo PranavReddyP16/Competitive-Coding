d4cf71c2bab60bc2618f1ab946f07250ec458e3c
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-01-21 18:11:19 -0500

    Add problem cf2124_B

---
+++ Bank/Unsolved/cf2124_B.cpp
@@ -0,0 +1,13 @@
+// dest: Codeforces/Div2/CF2124/
+// problem: CF2124 B
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    // TODO
+}
+signed main(){int t;cin>>t;while(t--)solve();}
