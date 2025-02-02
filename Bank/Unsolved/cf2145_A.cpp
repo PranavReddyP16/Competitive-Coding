b0833973b8cc9a94b82dd9ac9b0fb4f6cfd91e28
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-01 21:17:11 -0500

    Add problem cf2145_A

---
+++ Bank/Unsolved/cf2145_A.cpp
@@ -0,0 +1,13 @@
+// dest: Codeforces/Div2/CF2145/
+// problem: CF2145 A
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    // TODO
+}
+signed main(){int t;cin>>t;while(t--)solve();}
