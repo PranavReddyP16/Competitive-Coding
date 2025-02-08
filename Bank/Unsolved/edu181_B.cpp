da0296a3b3905a2b3260d0908342cb07252cd60a
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-07 22:03:33 -0500

    Add problem edu181_B

---
+++ Bank/Unsolved/edu181_B.cpp
@@ -0,0 +1,10 @@
+// dest: Codeforces/Educational/EDU181/
+// problem: EDU181 B
+#include<iostream>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    // TODO
+}
+signed main(){int t;cin>>t;while(t--)solve();}
