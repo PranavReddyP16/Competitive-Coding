3d2a42317aa53bbd3369f6dbbf64f3d3adc239c8
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-12 14:45:02 -0500

    Add problem practice_bs_advanced

---
+++ Bank/Unsolved/practice_bs_advanced.cpp
@@ -0,0 +1,10 @@
+// dest: Practice/
+// problem: bs_advanced
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    // TODO: bs_advanced
+}
+signed main(){int t;cin>>t;while(t--)solve();}
