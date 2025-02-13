13ed5c521b743dd1eeca977330980f183c11ce27
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-12 21:20:40 -0500

    Add problem practice_graph_advanced

---
+++ Bank/Unsolved/practice_graph_advanced.cpp
@@ -0,0 +1,10 @@
+// dest: Practice/
+// problem: graph_advanced
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    // TODO: graph_advanced
+}
+signed main(){int t;cin>>t;while(t--)solve();}
