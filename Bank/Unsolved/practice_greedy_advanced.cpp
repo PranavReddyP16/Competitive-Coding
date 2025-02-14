9800d79ae249495bdfbbe45784d14f257c3e8ae3
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-13 20:03:49 -0500

    Add problem practice_greedy_advanced

---
+++ Bank/Unsolved/practice_greedy_advanced.cpp
@@ -0,0 +1,10 @@
+// dest: Practice/
+// problem: greedy_advanced
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    // TODO: greedy_advanced
+}
+signed main(){int t;cin>>t;while(t--)solve();}
