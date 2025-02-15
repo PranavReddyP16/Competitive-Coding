4e2fc6024a80fe940ebad3f4d85cf5c3847661ea
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-15 13:48:51 -0500

    Add problem practice_string_advanced

---
+++ Bank/Unsolved/practice_string_advanced.cpp
@@ -0,0 +1,10 @@
+// dest: Practice/
+// problem: string_advanced
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    // TODO: string_advanced
+}
+signed main(){int t;cin>>t;while(t--)solve();}
