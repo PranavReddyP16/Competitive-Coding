20dbffe6e00cae13d36d06b028ea41892a4466d3
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-15 17:49:03 -0500

    Add CF1923 A

---
+++ Codeforces/Div2/CF1923/A.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    string s; cin>>s;
+    int cnt=0;
+    for(char c:s) if(c=='1') cnt++;
+    cout<<min(cnt, n-cnt)<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// 6828000
