4bbf0d2d52725bf6971aca238824d6b9b9dcda67
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-15 13:14:02 -0500

    Add problem practice_sort_advanced

---
+++ Bank/Unsolved/practice_sort_advanced.cpp
@@ -0,0 +1,10 @@
+// dest: Practice/
+// problem: sort_advanced
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    // TODO: sort_advanced
+}
+signed main(){int t;cin>>t;while(t--)solve();}
