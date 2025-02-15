ed07347dc0176fb75e23689e4b8d8eac8af293d4
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-15 18:32:30 -0500

    Add CF1923 B

---
+++ Codeforces/Div2/CF1923/B.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    string s; cin>>s;
+    sort(s.begin(),s.end());
+    string t=s;
+    reverse(t.begin(),t.end());
+    int ans=0;
+    for(int i=0;i<n;i++) ans+=abs(s[i]-t[i]);
+    cout<<ans<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// 7167000
