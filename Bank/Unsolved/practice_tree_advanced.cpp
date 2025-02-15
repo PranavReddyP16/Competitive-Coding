0ca8a822cd0685bca2b0dbf3aafe91b4f1c34931
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-15 15:28:02 -0500

    Add problem practice_tree_advanced

---
+++ Bank/Unsolved/practice_tree_advanced.cpp
@@ -0,0 +1,10 @@
+// dest: Practice/
+// problem: tree_advanced
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    // TODO: tree_advanced
+}
+signed main(){int t;cin>>t;while(t--)solve();}
