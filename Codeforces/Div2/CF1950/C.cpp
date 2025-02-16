28a386366f726c6f9ef0b4376f9ebbe196d2493f
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-16 14:25:15 -0500

    Add CF1950 C

---
+++ Codeforces/Div2/CF1950/C.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n); for(auto& x:a) cin>>x;
+    set<int> s;
+    for(int i=0;i<=n;i++) s.insert(i);
+    for(int x:a) s.erase(x);
+    cout<<*s.begin()<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// updated
