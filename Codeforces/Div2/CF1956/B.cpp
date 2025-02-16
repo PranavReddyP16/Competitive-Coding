cf0356f244e24510b47c66e8a70a689a2bd03f45
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-16 16:45:25 -0500

    Add CF1956 B

---
+++ Codeforces/Div2/CF1956/B.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    int n,m; cin>>n>>m;
+    vector<string> grid(n);
+    for(auto& s:grid) cin>>s;
+    int cnt=0;
+    for(int i=0;i<n;i++) for(int j=0;j<m;j++)
+        if((i+j)%2==0) cnt+=(grid[i][j]=='#');
+    cout<<cnt<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
