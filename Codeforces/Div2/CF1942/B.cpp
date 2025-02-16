87bbb66a90041cad505ceb6156d90b4c638ff1eb
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-16 11:57:06 -0500

    Add CF1942 B

---
+++ Codeforces/Div2/CF1942/B.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    int n,k; cin>>n>>k;
+    vector<int> a(n); for(auto& x:a) cin>>x;
+    int sum=0;
+    for(int i=0;i<k;i++) sum+=a[i];
+    int mx=sum;
+    for(int i=k;i<n;i++) {
+        sum+=a[i]-a[i-k];
+        mx=max(mx,sum);
+    }
+    cout<<mx<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// 6557000
