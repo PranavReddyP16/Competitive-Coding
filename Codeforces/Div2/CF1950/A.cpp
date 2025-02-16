77801f77cc51ef608d819bca28682eb5d71b0e38
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-16 13:32:15 -0500

    Add CF1950 A

---
+++ Codeforces/Div2/CF1950/A.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+void solve() {
+    int h,m; cin>>h>>m;
+    int total=h*60+m;
+    cout<<total/60<<" "<<total%60<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
