eee192fe4a433424e23b2306bf241480d25f3d5c
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-16 10:03:54 -0500

    Add CF1935 B

---
+++ Codeforces/Div2/CF1935/B.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n); for(auto& x:a) cin>>x;
+    int xr=0;
+    for(int i=0;i<n;i++) xr^=a[i];
+    if(xr==0) { cout<<"YES\n"; return; }
+    int cur=0; int cnt=0;
+    for(int i=0;i<n;i++) {
+        cur^=a[i];
+        if(cur==xr) { cnt++; cur=0; }
+    }
+    cout<<(cnt>=2?"YES":"NO")<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// 9311000
