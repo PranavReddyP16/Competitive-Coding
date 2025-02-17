1bdfb6ecf5d818173930db906c4b3019d82e097c
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-17 17:00:00 -0500

    Add CF1965 B

---
+++ Codeforces/Div2/CF1965/B.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    int total=n*(n+1)/2;
+    int missing; cin>>missing;
+    int sum=0;
+    for(int i=0;i<n-1;i++) { int x; cin>>x; sum+=x; }
+    cout<<total-sum<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// 7704000
