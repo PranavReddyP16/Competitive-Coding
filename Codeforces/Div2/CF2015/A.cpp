86e016d29c7b019416c79abcfffea8cc1f357dd0
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-02-28 22:31:38 -0500

    Add CF2015 A

---
+++ Codeforces/Div2/CF2015/A.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void solve() {
+    string s; cin>>s;
+    reverse(s.begin(),s.end());
+    cout<<s<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// 9282000
