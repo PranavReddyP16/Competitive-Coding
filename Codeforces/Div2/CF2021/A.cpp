3be83b71f3dc3a595bfdd0f3144cba5b2f459f40
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-01 21:11:37 -0500

    Add CF2021 A

---
+++ Codeforces/Div2/CF2021/A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n); for(auto& x:a) cin>>x;
+    sort(a.begin(),a.end());
+    if(n%2) cout<<a[n/2]<<"\n";
+    else cout<<(a[n/2-1]+a[n/2])/2<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// 3256000
+// 2396
+// 6964
