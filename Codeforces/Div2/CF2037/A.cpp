40d5448950d1f331b0331fe9359a1e1722477f6f
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-02 10:53:08 -0500

    Add CF2037 A

---
+++ Codeforces/Div2/CF2037/A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n); for(auto& x:a) cin>>x;
+    sort(a.begin(),a.end());
+    int cnt=0;
+    int l=0,r=n-1;
+    while(l<r) { if(a[l]+a[r]>0) { cnt++; l++; r--; } else l++; }
+    cout<<cnt<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// spring semester
+// 1106000
