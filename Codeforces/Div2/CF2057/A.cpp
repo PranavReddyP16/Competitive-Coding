7af46ebe0c5950d2fb3b0d5bbd6eabaa28091aea
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-03 15:32:26 -0500

    Add CF2057 A

---
+++ Codeforces/Div2/CF2057/A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve() {
+    int n,k; cin>>n>>k;
+    vector<int> a(n); for(auto& x:a) cin>>x;
+    int s=0;
+    for(int i=0;i<k;i++) s+=a[i];
+    int mx=s;
+    for(int i=k;i<n;i++) { s+=a[i]-a[i-k]; mx=max(mx,s); }
+    cout<<mx<<"\n";
+}
+signed main() { int t; cin>>t; while(t--) solve(); }
+// 3661000
