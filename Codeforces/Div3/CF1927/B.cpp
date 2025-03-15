7c0e250632bdc1732ee4a37b764d0ea2cd0aaf52
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-15 12:36:01 -0500

    Add CF1927 B (Div3)

---
+++ Codeforces/Div3/CF1927/B.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void solve(){string s;cin>>s;string t=s;reverse(t.begin(),t.end());cout<<(s==t?"YES":"NO")<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 1633000
+// 5622
