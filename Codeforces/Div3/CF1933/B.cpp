c6f0467c7fe83539ac24bc7f53b2b57ce210f47e
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-16 10:47:18 -0500

    Add CF1933 B (Div3)

---
+++ Codeforces/Div3/CF1933/B.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;int s=0;for(int i=0;i<n;i+=2)s+=a[i];cout<<s<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 7254000
