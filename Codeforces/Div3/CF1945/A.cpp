09855d266ae20738125ee52a99cfbd0aba593714
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-16 12:40:09 -0500

    Add CF1945 A (Div3)

---
+++ Codeforces/Div3/CF1945/A.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+#define int long long
+void solve(){int n;cin>>n;int a,b;cin>>a>>b;cout<<max(a,b)-min(a,b)+n<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
