efa73c6a25df7b5bc98b2aa10915a1278163efbf
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-21 21:44:23 -0500

    Add CF1999 B (Div3)

---
+++ Codeforces/Div3/CF1999/B.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;sort(a.begin(),a.end());cout<<a.back()-a[0]<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 8734000
+// 5337
