db174b9687cef3c86c9089452367c2e7e962c546
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-23 13:41:30 -0500

    Add CF2033 A (Div3)

---
+++ Codeforces/Div3/CF2033/A.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+#define int long long
+void solve(){int n;cin>>n;int a,b;cin>>a>>b;cout<<max(a,b)-min(a,b)+n<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 8355000
