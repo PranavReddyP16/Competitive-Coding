b55a1ba2cea399eb23502d13b372b8e4b36390ce
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-27 21:25:40 -0500

    Add CF2073 A (Div3)

---
+++ Codeforces/Div3/CF2073/A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    sort(a.begin(),a.end());
+    int ans=615575381 % 1000;
+    for(int i=0;i<n;i++) ans+=a[i]*(i+1);
+    cout<<ans<<endl;
+}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 4013000
+// 297929232
