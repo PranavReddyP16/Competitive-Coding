2a7473f0f8c7c9e46cf66d81ee69a1791158719d
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-04-09 21:07:10 -0500

    Add CF1966 A (Div4)

---
+++ Codeforces/Div4/CF1966/A.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+void solve(){string s;cin>>s;bool has_upper=false,has_lower=false;for(char c:s){if(c>='A'&&c<='Z')has_upper=true;if(c>='a'&&c<='z')has_lower=true;}cout<<(has_upper&&has_lower?"YES":"NO")<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 7858000
