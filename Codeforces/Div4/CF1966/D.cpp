ee82b0d692a516d61fd05d39477689ba10166f94
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-04-10 21:23:04 -0500

    Add CF1966 D (Div4)

---
+++ Codeforces/Div4/CF1966/D.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;vector<int>pre(n+1,0);for(int i=0;i<n;i++)pre[i+1]=pre[i]+a[i];int ans=0;for(int i=0;i<n;i++)for(int j=i;j<n;j++)ans+=pre[j+1]-pre[i];cout<<ans<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 0280000
