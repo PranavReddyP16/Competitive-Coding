1f5afb7d308af322e79eb265c775b4afe23a6d8c
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-04-13 11:20:16 -0500

    Add CF1986 B (Div4)

---
+++ Codeforces/Div4/CF1986/B.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;for(int i=0;i<n-1;i++)if(a[i]>a[i+1]&&(a[i]-a[i+1])%2!=0){cout<<"NO\n";return;}cout<<"YES\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
