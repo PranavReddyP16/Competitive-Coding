006ff3f9321e0e4215b41d9152e4b2807c8502de
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-04-13 15:02:38 -0500

    Add CF1986 C (Div4)

---
+++ Codeforces/Div4/CF1986/C.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+#define int long long
+void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;set<int>s(a.begin(),a.end());int mex=0;while(s.count(mex))mex++;cout<<mex<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 4214
