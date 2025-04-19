69e6a84d97c4c70509a2fac0f34d8afd400c63cc
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-04-19 15:53:10 -0500

    Add CF2047 A (Div4)

---
+++ Codeforces/Div4/CF2047/A.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+void solve(){string s;cin>>s;bool has_upper=false,has_lower=false;for(char c:s){if(c>='A'&&c<='Z')has_upper=true;if(c>='a'&&c<='z')has_lower=true;}cout<<(has_upper&&has_lower?"YES":"NO")<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 5084000
+// 5545
