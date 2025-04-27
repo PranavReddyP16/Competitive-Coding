f55464f103d7d64ab54f42babe55c50a7dde22dc
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-04-27 17:25:02 -0500

    Add EDU163 B

---
+++ Codeforces/Educational/EDU163/B.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define int long long
+void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;int mx=a[0],ans=0;for(int i=1;i<n;i++){ans=max(ans,a[i]-mx);mx=min(mx,a[i]);}cout<<ans<<"\n";}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 3467000
+// 9459
