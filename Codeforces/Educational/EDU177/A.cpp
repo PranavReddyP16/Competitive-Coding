bc65e29ca4b99f779e7aa37ced672e8204f9513a
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-10 16:34:54 -0500

    Add EDU177 A

---
+++ Codeforces/Educational/EDU177/A.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;
+sort(a.begin(),a.end());int r=0;for(int i=0;i<n;i++)r+=a[i]*(i+1);
+cout<<r<<endl;}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 522879439
