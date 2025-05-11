ba8c7570d7e71651836461913c36d00284a35038
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-10 20:38:10 -0500

    Solve LC 121_best_time_buy_sell

---
+++ LeetCode/Easy/121_best_time_buy_sell.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+int maxProfit(vector<int>&prices){
+    int mn=prices[0],mx=0;
+    for(int p:prices){mn=min(mn,p);mx=max(mx,p-mn);}
+    return mx;
+}};
+int main(){Solution s;vector<int>v={7,1,5,3,6,4};cout<<s.maxProfit(v);}
