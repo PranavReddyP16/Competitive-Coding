69893f5cb2e55b4d7c9e694ea6f4ca925e5d1d85
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-17 16:43:58 -0500

    Solve LC 124_binary_tree_max_path

---
+++ LeetCode/Hard/124_binary_tree_max_path.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode{int val;TreeNode*left,*right;TreeNode(int x):val(x),left(nullptr),right(nullptr){}};
+class Solution{public:
+int ans=INT_MIN;
+int dfs(TreeNode*r){if(!r)return 0;int l=max(0,dfs(r->left)),ri=max(0,dfs(r->right));ans=max(ans,l+ri+r->val);return max(l,ri)+r->val;}
+int maxPathSum(TreeNode*root){dfs(root);return ans;}
+};
+int main(){return 0;}
