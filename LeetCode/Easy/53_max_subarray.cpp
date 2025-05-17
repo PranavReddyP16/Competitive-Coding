22cdfe0556d3f3701980326f0827e6c64c907e4f
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-17 13:29:11 -0500

    Solve LC 53_max_subarray

---
+++ LeetCode/Easy/53_max_subarray.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+int maxSubArray(vector<int>&nums){
+    int mx=nums[0],cur=nums[0];
+    for(int i=1;i<(int)nums.size();i++){cur=max(nums[i],cur+nums[i]);mx=max(mx,cur);}
+    return mx;
+}};
+int main(){Solution s;vector<int>v={-2,1,-3,4,-1,2,1,-5,4};cout<<s.maxSubArray(v);}
+// 9872
