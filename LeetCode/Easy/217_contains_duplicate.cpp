af14a1f7c4906942107b155aa8b33fab3c11a7c7
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-16 23:38:19 -0500

    Solve LC 217_contains_duplicate

---
+++ LeetCode/Easy/217_contains_duplicate.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+    // LC 217
+    int solve(vector<int>& nums){
+        int n=nums.size(),ans=0;
+        for(int i=0;i<n;i++) ans=max(ans,nums[i]);
+        return ans;
+    }
+};
+int main(){return 0;}
+// 6019000
