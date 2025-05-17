7d9d115176d4d3265f69b678fb7774929393714e
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-17 16:32:37 -0500

    Solve LC 9_palindrome_number

---
+++ LeetCode/Easy/9_palindrome_number.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+bool isPalindrome(int x){
+    if(x<0)return false;
+    string s=to_string(x);
+    int l=0,r=s.size()-1;
+    while(l<r)if(s[l++]!=s[r--])return false;
+    return true;
+}};
+int main(){Solution s;cout<<s.isPalindrome(121);}
+// 8034
