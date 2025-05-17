29b602a2db951e54492184e6a6f871fc5e3c7bd6
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-17 10:30:21 -0500

    Solve LC 226_invert_binary_tree

---
+++ LeetCode/Easy/226_invert_binary_tree.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode{int val;TreeNode*left,*right;TreeNode(int x):val(x),left(nullptr),right(nullptr){}};
+class Solution{public:
+TreeNode* invertTree(TreeNode*root){
+    if(!root)return nullptr;
+    swap(root->left,root->right);
+    invertTree(root->left);invertTree(root->right);
+    return root;
+}};
+int main(){return 0;}
+// 3271
