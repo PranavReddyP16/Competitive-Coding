9a19addb8adbd8b87b13b7d5493cf7c80ac98af8
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-18 18:03:31 -0500

    Solve LC 42_trapping_rain_water

---
+++ LeetCode/Hard/42_trapping_rain_water.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+int trap(vector<int>&h){
+    int l=0,r=h.size()-1,lmax=0,rmax=0,ans=0;
+    while(l<r){if(h[l]<h[r]){lmax=max(lmax,h[l]);ans+=lmax-h[l];l++;}else{rmax=max(rmax,h[r]);ans+=rmax-h[r];r--;}}
+    return ans;
+}};
+int main(){Solution s;vector<int>v={0,1,0,2,1,0,1,3,2,1,2,1};cout<<s.trap(v);}
+// cleanup for github
