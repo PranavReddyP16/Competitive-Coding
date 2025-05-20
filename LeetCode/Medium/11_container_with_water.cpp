1fe01e5969249d8521f7d4d361047ce0cb315d09
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-19 20:56:28 -0500

    Solve LC 11_container_with_water

---
+++ LeetCode/Medium/11_container_with_water.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+int maxArea(vector<int>&h){
+    int l=0,r=h.size()-1,mx=0;
+    while(l<r){mx=max(mx,min(h[l],h[r])*(r-l));if(h[l]<h[r])l++;else r--;}
+    return mx;
+}};
+int main(){Solution s;vector<int>v={1,8,6,2,5,4,8,3,7};cout<<s.maxArea(v);}
