0e45c0145e3c2881759aa10505095599df6010f1
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-27 14:44:24 -0500

    Solve LC 322_coin_change

---
+++ LeetCode/Medium/322_coin_change.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+int coinChange(vector<int>&coins,int amount){
+    vector<int>dp(amount+1,amount+1);dp[0]=0;
+    for(int i=1;i<=amount;i++)for(int c:coins)if(c<=i)dp[i]=min(dp[i],dp[i-c]+1);
+    return dp[amount]>amount?-1:dp[amount];
+}};
+int main(){Solution s;vector<int>v={1,5,11};cout<<s.coinChange(v,15);}
+// winter break practice
