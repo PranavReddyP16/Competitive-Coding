d6bc4f0f5a558e83ccc73d877537652a9dc87cfd
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-27 22:51:09 -0500

    Solve LC 33_search_rotated

---
+++ LeetCode/Medium/33_search_rotated.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+int search(vector<int>&nums,int target){
+    int l=0,r=nums.size()-1;
+    while(l<=r){int m=(l+r)/2;
+        if(nums[m]==target)return m;
+        if(nums[l]<=nums[m]){if(target>=nums[l]&&target<nums[m])r=m-1;else l=m+1;}
+        else{if(target>nums[m]&&target<=nums[r])l=m+1;else r=m-1;}
+    }
+    return -1;
+}};
+int main(){Solution s;vector<int>v={4,5,6,7,0,1,2};cout<<s.search(v,0);}
