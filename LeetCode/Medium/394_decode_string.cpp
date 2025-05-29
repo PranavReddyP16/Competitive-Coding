f2c2de5fe590216c301129a82555c5326b7f5323
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-29 17:04:54 -0500

    Solve LC 394_decode_string

---
+++ LeetCode/Medium/394_decode_string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+    // LC 394
+    int solve(vector<int>& nums){
+        int n=nums.size(),ans=0;
+        for(int i=0;i<n;i++) ans=max(ans,nums[i]);
+        return ans;
+    }
+};
+int main(){return 0;}
+// 7728000
+// 6446
