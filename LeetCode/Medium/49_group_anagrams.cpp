10ecba2419c207bfb036e2d1626b9cfdce274f7e
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-05-31 12:05:18 -0500

    Solve LC 49_group_anagrams

---
+++ LeetCode/Medium/49_group_anagrams.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+vector<vector<string>> groupAnagrams(vector<string>&strs){
+    unordered_map<string,vector<string>>m;
+    for(auto&s:strs){string k=s;sort(k.begin(),k.end());m[k].push_back(s);}
+    vector<vector<string>>res;
+    for(auto&[k,v]:m)res.push_back(v);
+    return res;
+}};
+int main(){return 0;}
