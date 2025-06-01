4f1c1626011e1ad1e32fc6d124ff479cc18ef7e5
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-06-01 14:19:28 -0500

    Solve LC 78_subsets

---
+++ LeetCode/Medium/78_subsets.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+vector<vector<int>> subsets(vector<int>&nums){
+    int n=nums.size();vector<vector<int>>res;
+    for(int mask=0;mask<(1<<n);mask++){
+        vector<int>sub;
+        for(int i=0;i<n;i++)if(mask&(1<<i))sub.push_back(nums[i]);
+        res.push_back(sub);
+    }
+    return res;
+}};
+int main(){Solution s;vector<int>v={1,2,3};cout<<s.subsets(v).size();}
