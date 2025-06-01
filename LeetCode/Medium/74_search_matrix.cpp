79e0092c6c8689a55a73e0d48d9ec7f87509454f
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-06-01 13:54:48 -0500

    Solve LC 74_search_matrix

---
+++ LeetCode/Medium/74_search_matrix.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{public:
+int solve(vector<int>&v){int n=v.size(),r=0;for(int i=0;i<n;i++)r=max(r,v[i]);return r;}
+};
+int main(){return 0;}
+// 3226770617
