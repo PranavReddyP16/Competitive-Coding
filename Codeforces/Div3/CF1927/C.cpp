761f27e2ef8b29891ba9918a4364320644081307
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-08-03 17:59:48 -0500

    Fix C

--- Codeforces/Div3/CF1927/C.cpp
+++ Codeforces/Div3/CF1927/C.cpp
@@ -7,3 +7,4 @@ using namespace std;
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;map<int,int>f;for(int x:a)f[x]++;vector<pair<int,int>>v(f.begin(),f.end());sort(v.begin(),v.end(),[](auto&a,auto&b){return a.second>b.second;});for(auto&[val,cnt]:v)for(int i=0;i<cnt;i++)cout<<val<<" ";cout<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
 // 4167000
+// 5617
