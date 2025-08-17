efc9caab9da11e93604177ef4702a8db5ad805a3
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-08-16 20:22:20 -0500

    Refactor B

--- Codeforces/Div3/CF2048/B.cpp
+++ Codeforces/Div3/CF2048/B.cpp
@@ -6,3 +6,4 @@ using namespace std;
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;sort(a.begin(),a.end());cout<<a.back()-a[0]<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
 // 6136000
+// 2398
