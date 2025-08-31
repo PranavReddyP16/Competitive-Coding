ff5dc2cb4416c6656ee6ee158b631503d0b2ec89
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-08-30 21:33:12 -0500

    Clean B

--- Codeforces/Educational/EDU169/B.cpp
+++ Codeforces/Educational/EDU169/B.cpp
@@ -5,3 +5,4 @@ using namespace std;
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;int mx=a[0],ans=0;for(int i=1;i<n;i++){ans=max(ans,a[i]-mx);mx=min(mx,a[i]);}cout<<ans<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
 // 9188000
+// 7348
