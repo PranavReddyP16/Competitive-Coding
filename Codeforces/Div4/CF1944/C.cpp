b74c5cff8b7fa09b7bb310e82ad85aaa8ad8bb24
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-10-04 16:07:58 -0500

    Fix C

--- Codeforces/Div4/CF1944/C.cpp
+++ Codeforces/Div4/CF1944/C.cpp
@@ -5,3 +5,4 @@ using namespace std;
 #define int long long
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;set<int>s(a.begin(),a.end());int mex=0;while(s.count(mex))mex++;cout<<mex<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
+// 7761
