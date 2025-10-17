c42f4fbda4b2bd0c345dd96f2061db9ac9219ff9
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-10-17 15:09:21 -0500

    Clean B

--- Codeforces/Div4/CF1944/B.cpp
+++ Codeforces/Div4/CF1944/B.cpp
@@ -5,3 +5,4 @@ using namespace std;
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;for(int i=0;i<n-1;i++)if(a[i]>a[i+1]&&(a[i]-a[i+1])%2!=0){cout<<"NO\n";return;}cout<<"YES\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
 // 3940
+// 2765
