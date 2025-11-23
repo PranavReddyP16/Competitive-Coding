e2d3101732fad826e99c6c4a19ccf20831236420
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-11-23 18:52:52 -0500

    Improve B

--- Codeforces/Div3/CF1933/B.cpp
+++ Codeforces/Div3/CF1933/B.cpp
@@ -5,3 +5,4 @@ using namespace std;
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;int s=0;for(int i=0;i<n;i+=2)s+=a[i];cout<<s<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
 // 7254000
+// 7264
