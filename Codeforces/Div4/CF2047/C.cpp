3b4ccdef12df8b354bb7d1bf5ffc4ebbef3a36ec
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-12-23 19:07:44 -0500

    Optimize C

--- Codeforces/Div4/CF2047/C.cpp
+++ Codeforces/Div4/CF2047/C.cpp
@@ -6,3 +6,4 @@ using namespace std;
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;set<int>s(a.begin(),a.end());int mex=0;while(s.count(mex))mex++;cout<<mex<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
 // 8035
+// 4712
