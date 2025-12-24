07a656718b08af0383ee9088bbca3d67940292b9
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-12-23 21:56:02 -0500

    Fix B

--- Codeforces/Div3/CF1971/B.cpp
+++ Codeforces/Div3/CF1971/B.cpp
@@ -5,3 +5,4 @@ using namespace std;
 #define int long long
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;sort(a.begin(),a.end());cout<<a.back()-a[0]<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
+// 7354
