e150075085c9cac4f3f94f469917f7414127795f
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2026-01-01 17:18:13 -0500

    Clean A

--- Codeforces/Div4/CF1986/A.cpp
+++ Codeforces/Div4/CF1986/A.cpp
@@ -3,3 +3,4 @@ using namespace std;
 void solve(){string s;cin>>s;bool has_upper=false,has_lower=false;for(char c:s){if(c>='A'&&c<='Z')has_upper=true;if(c>='a'&&c<='z')has_lower=true;}cout<<(has_upper&&has_lower?"YES":"NO")<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
 // 9773
+// 8968
