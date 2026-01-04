b64ce2ba39f328d6876c68275c0c2c233a8a1efe
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2026-01-04 14:30:32 -0500

    Clean D

--- Codeforces/Div4/CF1986/D.cpp
+++ Codeforces/Div4/CF1986/D.cpp
@@ -5,3 +5,4 @@ using namespace std;
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;vector<int>pre(n+1,0);for(int i=0;i<n;i++)pre[i+1]=pre[i]+a[i];int ans=0;for(int i=0;i<n;i++)for(int j=i;j<n;j++)ans+=pre[j+1]-pre[i];cout<<ans<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
 // 3397
+// 8116
