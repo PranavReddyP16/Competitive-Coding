21dcf2249466369bbe7eac6083379dd950bad2de
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2026-01-24 18:48:34 -0500

    Improve D

--- Codeforces/Div4/CF2047/D.cpp
+++ Codeforces/Div4/CF2047/D.cpp
@@ -4,3 +4,4 @@ using namespace std;
 #define int long long
 void solve(){int n;cin>>n;vector<int>a(n);for(auto&x:a)cin>>x;vector<int>pre(n+1,0);for(int i=0;i<n;i++)pre[i+1]=pre[i]+a[i];int ans=0;for(int i=0;i<n;i++)for(int j=i;j<n;j++)ans+=pre[j+1]-pre[i];cout<<ans<<"\n";}
 signed main(){int t;cin>>t;while(t--)solve();}
+// 2457
