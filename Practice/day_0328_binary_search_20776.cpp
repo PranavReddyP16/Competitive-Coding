d619598387a846adf0c7e2ba14b1cfb84426e3bf
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-03-28 15:52:55 +0000

    Practice: binary_search

---
+++ Practice/day_0328_binary_search_20776.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int W;cin>>W;vector<int>w(n),v(n);for(int i=0;i<n;i++)cin>>w[i]>>v[i];vector<int>dp(W+1,0);for(int i=0;i<n;i++)for(int j=W;j>=w[i];j--)dp[j]=max(dp[j],dp[j-w[i]]+v[i]);cout<<dp[W]<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 7878
