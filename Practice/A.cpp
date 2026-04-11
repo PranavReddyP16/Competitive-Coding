a24bbab9bc69ddf2212bab020627ff9fa485b6e3
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-04-11 15:53:55 +0000

    Solve sort_advanced

--- Practice/A.cpp
+++ Practice/A.cpp
@@ -7,7 +7,9 @@ void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    int mx=a[n-1];cout<<mx<<" ";for(int i=n-2;i>=0;i--){if(a[i]>=mx){cout<<a[i]<<" ";mx=a[i];}}cout<<"
+    sort(a.begin(),a.end());int c=0;for(int i=1;i<n;i++)if(a[i]==a[i-1])c++;cout<<c<<"
 ";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
+// 6011
+// 6247
