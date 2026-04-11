a0d3a064b5114cf96235c0c18371098f2b156b45
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-04-11 15:53:55 +0000

    Solve CF2127 A

--- Codeforces/Div2/CF2127/A.cpp
+++ Codeforces/Div2/CF2127/A.cpp
@@ -1,13 +1,16 @@
-// dest: Codeforces/Div2/CF2127/
-// problem: CF2127 A
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    vector<int>dp;for(int x:a){auto it=lower_bound(dp.begin(),dp.end(),x);if(it==dp.end())dp.push_back(x);else *it=x;}cout<<dp.size()<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
+// 6304
+// 6234
+// 5429
