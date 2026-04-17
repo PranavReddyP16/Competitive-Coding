d899cb968c8d630af4b69b8327cde3d3bd77f341
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-04-17 16:29:12 +0000

    Solve CF2132 B

--- Codeforces/Div2/CF2132/B.cpp
+++ Codeforces/Div2/CF2132/B.cpp
@@ -1,15 +1,15 @@
-// dest: Codeforces/Div2/CF2132/
-// problem: CF2132 B
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    int x;cin>>x;int a_=0,b_=1;for(int i=2;i<=x;i++){int c=a_+b_;a_=b_;b_=c;}cout<<b_<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 3544
-// 5252
+// 3172
+// 4365
