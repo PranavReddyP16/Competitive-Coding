6e71320fb9e1e9280f339796a1c255d680e26f38
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-04-18 15:58:55 +0000

    Solve CF2117 B

--- Codeforces/Div2/CF2117/B.cpp
+++ Codeforces/Div2/CF2117/B.cpp
@@ -1,14 +1,15 @@
-// dest: Codeforces/Div2/CF2117/
-// problem: CF2117 B
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    int x;cin>>x;bool ok=x>1;for(int i=2;i*i<=x;i++)if(x%i==0)ok=false;cout<<(ok?"YES":"NO")<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 2650
+// 2070
+// 6255
