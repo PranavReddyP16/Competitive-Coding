bfd14c84ac745aa919106eecb14df0ae3bcaea8f
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-04-23 16:57:49 +0000

    Fix A

--- Codeforces/Div4/CF2007/A.cpp
+++ Codeforces/Div4/CF2007/A.cpp
@@ -4,3 +4,4 @@ void solve(){string s;cin>>s;bool has_upper=false,has_lower=false;for(char c:s){
 signed main(){int t;cin>>t;while(t--)solve();}
 // 3561000
 // 7812
+// cleanup: removed unused variables
