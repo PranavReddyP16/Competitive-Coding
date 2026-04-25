b72fe3583bdde9f7326403131d7f1b064ff2e190
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-04-25 16:01:50 +0000

    Solve CF2119 A

--- Codeforces/Div2/CF2119/A.cpp
+++ Codeforces/Div2/CF2119/A.cpp
@@ -1,15 +1,15 @@
-// dest: Codeforces/Div2/CF2119/
-// problem: CF2119 A
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    int x;cin>>x;int a_=0,b_=1;for(int i=2;i<=x;i++){int c=a_+b_;a_=b_;b_=c;}cout<<b_<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 9002
-// 9245
+// 3172
+// 4365
