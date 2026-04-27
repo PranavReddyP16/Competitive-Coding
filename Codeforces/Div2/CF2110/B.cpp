6231fbcfe80cf5a666cbb981742a861817544c67
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-04-27 17:03:12 +0000

    Solve CF2110 B

--- Codeforces/Div2/CF2110/B.cpp
+++ Codeforces/Div2/CF2110/B.cpp
@@ -1,14 +1,16 @@
-// dest: Codeforces/Div2/CF2110/
-// problem: CF2110 B
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    string s;cin>>s;int f[256]={};for(char c:s)f[(int)c]++;for(char c:s)if(f[(int)c]==1){cout<<c<<"
+";return;}cout<<"-1
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 1889
+// 5939
+// 1563
