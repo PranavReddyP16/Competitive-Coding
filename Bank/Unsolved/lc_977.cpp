3c78e34e8aefca7976ba562f5c68293ce2b2abee
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-04-28 17:09:34 +0000

    Solve LC 977

--- Bank/Unsolved/lc_977.cpp
+++
@@ -1,8 +0,0 @@
-// dest: LeetCode/Hard/
-// problem: LC 977
-#include<bits/stdc++.h>
-using namespace std;
-class Solution{public:
-    // TODO
-};
-int main(){return 0;}
--- LeetCode/Hard/A.cpp
+++ LeetCode/Hard/A.cpp
@@ -7,8 +7,9 @@ void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    vector<int>b(n);for(auto&x:b)cin>>x;vector<int>ev;for(int i=0;i<n;i++){ev.push_back(a[i]);ev.push_back(b[i]);}sort(ev.begin(),ev.end());int cur=0,mx=0;for(int i=0;i<n;i++){cur++;mx=max(mx,cur);}cout<<mx<<"
+    int x;cin>>x;bool ok=x>1;for(int i=2;i*i<=x;i++)if(x%i==0)ok=false;cout<<(ok?"YES":"NO")<<"
 ";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 9976
+// 2070
+// 6255
