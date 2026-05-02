64ab5821b3c3255e3ff0f526a08eed08b20a8d61
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-05-02 16:07:05 +0000

    Practice: sorting

---
+++ Practice/day_0502_sorting_12050.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    string s,t;cin>>s>>t;sort(s.begin(),s.end());sort(t.begin(),t.end());cout<<(s==t?"YES":"NO")<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 4239
+// 6139
