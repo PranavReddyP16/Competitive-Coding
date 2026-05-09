d6fe8b18e52e928f9bfd8f813f297586b5190b6e
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-05-09 16:12:46 +0000

    Solve CF2147 A

--- Codeforces/Div2/CF2147/A.cpp
+++ Codeforces/Div2/CF2147/A.cpp
@@ -1,13 +1,15 @@
-// dest: Codeforces/Div2/CF2147/
-// problem: CF2147 A
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    int s=0;for(int x:a)if(x%2==0)s+=x;cout<<s<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
+// 9665
+// 3446
