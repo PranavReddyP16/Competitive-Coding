b7fb9eb0e1a1e79603a01a7c4b5e40092ac30a6b
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-05-10 16:27:27 +0000

    Solve CF2150 B

--- Codeforces/Div2/CF2150/B.cpp
+++ Codeforces/Div2/CF2150/B.cpp
@@ -1,14 +1,15 @@
-// dest: Codeforces/Div2/CF2150/
-// problem: CF2150 B
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    sort(a.begin(),a.end());int c=0;for(int i=1;i<n;i++)if(a[i]==a[i-1])c++;cout<<c<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 1917
+// 6011
+// 6247
