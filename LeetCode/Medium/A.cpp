18efea7f4c792a021d712873fa6766911260af38
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-05-17 16:30:06 +0000

    Solve LC 680

--- LeetCode/Medium/A.cpp
+++ LeetCode/Medium/A.cpp
@@ -7,8 +7,9 @@ void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    int m;cin>>m;vector<vector<int>>adj(n);for(int i=0;i<m;i++){int u,v;cin>>u>>v;adj[u].push_back(v);adj[v].push_back(u);}vector<bool>vis(n,false);int comp=0;function<void(int)>dfs=[&](int u){vis[u]=true;for(int v:adj[u])if(!vis[v])dfs(v);};for(int i=0;i<n;i++)if(!vis[i]){comp++;dfs(i);}cout<<comp<<"
+    int x;cin>>x;int a_=0,b_=1;for(int i=2;i<=x;i++){int c=a_+b_;a_=b_;b_=c;}cout<<b_<<"
 ";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 7139
+// 3172
+// 4365
