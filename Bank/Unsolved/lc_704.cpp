7943da29e42b923e8438ffaaee8d1ed0acf3ea59
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-05-27 18:09:02 +0000

    Solve LC 704

--- Bank/Unsolved/lc_704.cpp
+++
@@ -1,8 +0,0 @@
-// dest: LeetCode/Medium/
-// problem: LC 704
-#include<bits/stdc++.h>
-using namespace std;
-class Solution{public:
-    // TODO
-};
-int main(){return 0;}
--- LeetCode/Medium/A.cpp
+++ LeetCode/Medium/A.cpp
@@ -7,9 +7,7 @@ void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    int x;cin>>x;int a_=0,b_=1;for(int i=2;i<=x;i++){int c=a_+b_;a_=b_;b_=c;}cout<<b_<<"
-";
+    sort(a.begin(),a.end());int q;cin>>q;while(q--){int x;cin>>x;cout<<(binary_search(a.begin(),a.end(),x)?"YES":"NO")<<"
+";}
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 3172
-// 4365
