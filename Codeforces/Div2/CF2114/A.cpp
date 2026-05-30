e597bf78f9f4064ee8cd60849ec0e4bac7ebd773
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-05-30 16:32:59 +0000

    Solve CF2114 A

--- Codeforces/Div2/CF2114/A.cpp
+++ Codeforces/Div2/CF2114/A.cpp
@@ -1,15 +1,13 @@
-// dest: Codeforces/Div2/CF2114/
-// problem: CF2114 A
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    map<int,int>f;for(int x:a)f[x]++;vector<pair<int,int>>v;for(auto&[k,c]:f)v.push_back({c,k});sort(v.rbegin(),v.rend());for(auto&[c,k]:v)for(int i=0;i<c;i++)cout<<k<<" ";cout<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 7767
-// 6770
