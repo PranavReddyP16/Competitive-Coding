e5a5a07645d10fbb21492f5bc0f93e2a436d86fd
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-06-07 16:49:26 +0000

    Solve CF2142 B

--- Codeforces/Div2/CF2142/B.cpp
+++ Codeforces/Div2/CF2142/B.cpp
@@ -1,13 +1,16 @@
-// dest: Codeforces/Div2/CF2142/
-// problem: CF2142 B
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    sort(a.begin(),a.end());int x;cin>>x;auto it=lower_bound(a.begin(),a.end(),x);int fl=it!=a.begin()?*prev(it):-1;int cl=it!=a.end()?*it:-1;cout<<fl<<" "<<cl<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
+// 9399
+// 6846
+// 9757
