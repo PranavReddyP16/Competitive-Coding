3ba1bb50388627076d4631dd75a307c6f533ff84
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-06-18 18:13:47 +0000

    Solve CF2124 B

--- Codeforces/Div2/CF2124/B.cpp
+++ Codeforces/Div2/CF2124/B.cpp
@@ -1,14 +1,16 @@
-// dest: Codeforces/Div2/CF2124/
-// problem: CF2124 B
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    int s=0;for(int x:a){while(x)s+=x%10,x/=10;}cout<<s<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 6137
+// 3404
+// 4497
+// 8680
