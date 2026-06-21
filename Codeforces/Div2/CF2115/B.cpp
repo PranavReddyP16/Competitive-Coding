3da1d0f8196c039c9a44c8ee4d0ede7dee0d520f
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-06-21 17:00:16 +0000

    Solve CF2115 B

--- Codeforces/Div2/CF2115/B.cpp
+++ Codeforces/Div2/CF2115/B.cpp
@@ -1,13 +1,13 @@
-// dest: Codeforces/Div2/CF2115/
-// problem: CF2115 B
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    sort(a.begin(),a.end());int q;cin>>q;while(q--){int x;cin>>x;cout<<(binary_search(a.begin(),a.end(),x)?"YES":"NO")<<"
+";}
 }
 signed main(){int t;cin>>t;while(t--)solve();}
