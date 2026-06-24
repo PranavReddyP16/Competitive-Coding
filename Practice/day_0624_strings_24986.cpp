dab931f3220d94737fec1dbcc45d73085657f8d8
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-06-24 17:25:11 +0000

    Practice: strings

---
+++ Practice/day_0624_strings_24986.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    int k;cin>>k;k%=n;rotate(a.begin(),a.begin()+n-k,a.end());for(int x:a)cout<<x<<" ";cout<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 8206
+// 1189
