7bcfc1e9582f4d688504e223a2f4dfadbc17cc17
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-06-26 17:13:50 +0000

    Solve CF2145 B

--- Codeforces/Div2/CF2145/B.cpp
+++ Codeforces/Div2/CF2145/B.cpp
@@ -1,16 +1,15 @@
-// dest: Codeforces/Div2/CF2145/
-// problem: CF2145 B
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    int total=0;for(int x:a)total+=x;int left=0;for(int i=0;i<n;i++){if(left==total-left-a[i]){cout<<i<<"
+";return;}left+=a[i];}cout<<-1<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 4581
-// 6298
-// 4210
+// 3155
