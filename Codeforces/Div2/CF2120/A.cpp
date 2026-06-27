b6f2a1c0965a1c7be15b10571af82b686100025f
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-06-27 16:36:09 +0000

    Solve CF2120 A

--- Codeforces/Div2/CF2120/A.cpp
+++ Codeforces/Div2/CF2120/A.cpp
@@ -1,15 +1,13 @@
-// dest: Codeforces/Div2/CF2120/
-// problem: CF2120 A
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    string s;cin>>s;string r;int i=0;while(i<(int)s.size()){int j=i;while(j<(int)s.size()&&s[j]==s[i])j++;r+=s[i];r+=to_string(j-i);i=j;}cout<<r<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 4931
-// 3572
