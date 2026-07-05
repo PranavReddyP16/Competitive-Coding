769c460067c3e19ca3d8e971dc252fb5a4b88de5
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-07-05 16:32:09 +0000

    Solve string_advanced

--- Practice/A.cpp
+++ Practice/A.cpp
@@ -7,7 +7,8 @@ void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    int mx=a[n-1];cout<<mx<<" ";for(int i=n-2;i>=0;i--){if(a[i]>=mx){cout<<a[i]<<" ";mx=a[i];}}cout<<"
+    int m;cin>>m;vector<vector<int>>adj(n);for(int i=0;i<m;i++){int u,v;cin>>u>>v;adj[u].push_back(v);adj[v].push_back(u);}vector<bool>vis(n,false);int comp=0;function<void(int)>dfs=[&](int u){vis[u]=true;for(int v:adj[u])if(!vis[v])dfs(v);};for(int i=0;i<n;i++)if(!vis[i]){comp++;dfs(i);}cout<<comp<<"
 ";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
+// 7139
