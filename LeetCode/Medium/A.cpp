d006664962e231db8a1972932386d6e8b6ed53ab
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-07-25 16:24:42 +0000

    Solve LC 643

--- LeetCode/Medium/A.cpp
+++ LeetCode/Medium/A.cpp
@@ -7,9 +7,12 @@ void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    sort(a.begin(),a.end());int c=0;for(int i=1;i<n;i++)if(a[i]==a[i-1])c++;cout<<c<<"
+    reverse(a.begin(),a.end());for(int x:a)cout<<x<<" ";cout<<"
 ";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 6011
-// 6247
+// 3974
+// 9043
+// 8575
+// 9526
+// 2045
