367c52ee34bf4bef3a07d3d25d2a5c7c59f7b73a
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-08-01 16:27:18 +0000

    Practice: sorting

---
+++ Practice/day_0801_sorting_27481.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int long long
+void solve() {
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto& x:a) cin>>x;
+    string s;cin>>s;int c=0;for(char ch:s)if(string("aeiou").find(ch)!=string::npos)c++;cout<<c<<"
+";
+}
+signed main(){int t;cin>>t;while(t--)solve();}
+// 4178
