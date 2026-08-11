148f23247b344bbac0e95577880c9da53c60fe87
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-08-11 16:22:22 +0000

    Solve math_advanced

--- Practice/A.cpp
+++ Practice/A.cpp
@@ -7,9 +7,7 @@ void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    string s;cin>>s;int f[26]={};for(char c:s)f[c- ' a ' ]++;for(int i=0;i<26;i++)if(f[i])cout<<(char)( ' a ' +i)<<":"<<f[i]<<" ";cout<<"
+    string s;cin>>s;int len=s.size();vector<vector<int>>dp(len,vector<int>(len,0));for(int l=2;l<=len;l++)for(int i=0;i<=len-l;i++){int j=i+l-1;dp[i][j]=s[i]==s[j]?dp[i+1][j-1]:min(dp[i+1][j],dp[i][j-1])+1;}cout<<dp[0][len-1]<<"
 ";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 4419
-// 8391
