f29aa21c499472481f43def445f48964df90ae3d
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-08-15 15:42:26 +0000

    Fix A

--- AtCoder/ARC/ARC181/A.cpp
+++ AtCoder/ARC/ARC181/A.cpp
@@ -5,3 +5,4 @@ using namespace std;
 const int MOD=998244353;
 signed main(){int n;cin>>n;int ans=1;for(int i=0;i<n;i++)ans=ans*2%MOD;cout<<ans<<"\n";}
 // 4549
+// cleanup: removed unused variables
