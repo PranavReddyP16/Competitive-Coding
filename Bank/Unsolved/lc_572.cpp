61ff88fa9dadd72344539adce6bc1271b088fcda
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-08-29 18:43:33 +0000

    Solve LC 572

--- Bank/Unsolved/lc_572.cpp
+++
@@ -1,8 +0,0 @@
-// dest: LeetCode/Easy/
-// problem: LC 572
-#include<bits/stdc++.h>
-using namespace std;
-class Solution{public:
-    // TODO
-};
-int main(){return 0;}
--- LeetCode/Easy/A.cpp
+++ LeetCode/Easy/A.cpp
@@ -7,7 +7,9 @@ void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    int k;cin>>k;sort(a.begin(),a.end());cout<<a[k-1]<<"
+    sort(a.begin(),a.end());int c=0;for(int i=1;i<n;i++)if(a[i]==a[i-1])c++;cout<<c<<"
 ";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
+// 6011
+// 6247
