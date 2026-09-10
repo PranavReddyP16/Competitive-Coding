8a2166dc14e0048f47acec70d98e49d05ae60a42
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-09-10 18:35:28 +0000

    Solve CF2135 B

--- Codeforces/Div2/CF2135/B.cpp
+++ Codeforces/Div2/CF2135/B.cpp
@@ -1,13 +1,14 @@
-// dest: Codeforces/Div2/CF2135/
-// problem: CF2135 B
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 #define int long long
 void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    // TODO
+    int mn=a[0],mx=0;for(int x:a){mn=min(mn,x);mx=max(mx,x-mn);}cout<<mx<<"
+";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
+// 3288
