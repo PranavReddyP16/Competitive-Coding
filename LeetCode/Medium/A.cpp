dd27def6ae25c911f62ce3cb527e30367d84c14b
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-09-20 18:14:26 +0000

    Solve LC 876

--- LeetCode/Medium/A.cpp
+++ LeetCode/Medium/A.cpp
@@ -7,9 +7,10 @@ void solve() {
     int n; cin>>n;
     vector<int> a(n);
     for(auto& x:a) cin>>x;
-    int total=0;for(int x:a)total+=x;int left=0;for(int i=0;i<n;i++){if(left==total-left-a[i]){cout<<i<<"
-";return;}left+=a[i];}cout<<-1<<"
+    int s=0;for(int x:a){while(x)s+=x%10,x/=10;}cout<<s<<"
 ";
 }
 signed main(){int t;cin>>t;while(t--)solve();}
-// 3155
+// 3404
+// 4497
+// 8680
