b4307b3f622cba2cae0baaad0594d59acccd804b
PranavReddyP16 <pranavreddyp16@gmail.com>
2026-09-20 18:14:26 +0000

    Optimize lca

--- Templates/lca.cpp
+++ Templates/lca.cpp
@@ -46,3 +46,4 @@ int main() {
 // Note: O(log n) per operation, O(n) build
 // Updated: simplified merge function
 // Variant: supports range update with lazy propagation
+// Variant: supports range update with lazy propagation
